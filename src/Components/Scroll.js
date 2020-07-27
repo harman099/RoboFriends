251214bdf830a915f39ea6aca67afb661f3e71af
harman099 <harmansinghpaul@gmail.com>
2020-07-27 12:58:21 +0530

    Optimising

--- src/Components/Scroll.js
+++ src/Components/Scroll.js
@@ -1,16 +1,11 @@
-import React from 'react';
+import React from "react";
 
 //Children - So Scroll becomes parent of CardList therefore holds all the props of CardList
 
 const Scroll = (props) => {
-	return(
-		<div 
-		style = {{overflowY:'scroll', 
-		height: '500px',
-	}}>
-			{props.children}
-		</div>
-		);
+  return (
+    <div style={{ overflowY: "scroll", height: "600px" }}>{props.children}</div>
+  );
 };
 
 export default Scroll;
