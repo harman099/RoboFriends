9f16f113a0bb6789cb5d3bf3b8192460192fa089
harman099 <harmansinghpaul@gmail.com>
2020-07-27 13:51:34 +0530

    Added registerServiceWorker.js

--- src/Components/Scroll.js
+++ src/Components/Scroll.js
@@ -4,7 +4,11 @@ import React from "react";
 
 const Scroll = (props) => {
   return (
-    <div style={{ overflowY: "scroll", height: "600px" }}>{props.children}</div>
+    <div
+      style={{ overflow: "scroll", border: "5px solid black", height: "800px" }}
+    >
+      {props.children}
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { createLogger } from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
-import './index.css';
-import App from './Containers/App.js'
-import * as serviceWorker from './serviceWorker';
-import { searchRobots, requestRobots } from './Reducers.js';
-import 'tachyons';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import "./index.css";
+import App from "./Containers/App.js";
+import { searchRobots, requestRobots } from "./Reducers.js";
+import "tachyons";
+import registerServiceWorker from "./registerServiceWorker";
 
 const logger = createLogger();
-const rootReducer = combineReducers({searchRobots, requestRobots})
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+);
 
-ReactDOM.render(<Provider store = {store}>
-	<App />
-	</Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+registerServiceWorker();
